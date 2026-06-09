7b684517b34b4874b03ddac263a142e4f44b39e2
Diego Castillo <diegosebastian@MacBook-Air-de-Diego.local>
2026-06-08 22:04:41 -0400

    Despliegue a GitHub con la primera API para ver todas las ubicaciones en formato JSON y agregar una nueva ubicacion a través de un POST

--- principal/urls.py
+++ principal/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     #path('', views.inicio, name='inicio'),
     path('', views.crear_ubicacion, name='crear_ubicacion'),
-    path('mapa/', views.mapa, name='mapa')
+    path('mapa/', views.mapa, name='mapa'),
+    path("api/ubicaciones/", views.obtener_ubicaciones, name="APIubicaciones"),
+    path("api/add-ubi/", views.inserta_ubicacion, name="APInueva"),
 ]
--- principal/views.py
+++ principal/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .forms import UbicacionForm
 from .models import ubicacion
+import json
 
 def inicio(request):
-
-
     return HttpResponse("Prueba 1")
 
 def crear_ubicacion(request):
@@ -31,3 +31,44 @@ def mapa(request):
     return render(request, 'mapa.html', {
         'ubicaciones': ubicaciones
     })
+
+# APIs ===========================================================
+def obtener_ubicaciones(request):
+    ubicaciones = ubicacion.objects.all().values(
+        "id_ubi",
+        "latitud",
+        "longitud"
+    )
+
+    return JsonResponse({
+        "status": 200,
+        "ubicaciones": list(ubicaciones)
+    })
+
+@csrf_exempt
+def inserta_ubicacion(request):
+
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+
+            nueva = ubicacion.objects.create(
+                latitud=data["latitud"],
+                longitud=data["longitud"]
+            )
+
+            return JsonResponse({
+                "status": 201,
+                "id_ubi": nueva.id_ubi
+            })
+
+        except Exception as ex:
+            return JsonResponse({
+                "status": 400,
+                "error": str(ex)
+            }, status=400)
+
+    return JsonResponse({
+        "status": 405,
+        "mensaje": "Método no permitido"
+    }, status=405)
